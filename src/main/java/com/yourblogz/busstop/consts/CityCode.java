ed3112875012bd05dd17612ac7159868f0044b26
will.leung <ewill.leung@gmail.com>
2015-12-08 18:18:55 +0800

    Add CityCode

---
+++ src/main/java/com/yourblogz/busstop/consts/CityCode.java
@@ -0,0 +1,692 @@
+package com.yourblogz.busstop.consts;
+
+public interface CityCode {
+    /* 拉萨市 */
+    static int C100 = 100;
+    /* 那曲地区 */
+    static int C101 = 101;
+    /* 日喀则地区 */
+    static int C102 = 102;
+    /* 阿里地区 */
+    static int C103 = 103;
+    /* 昆明市 */
+    static int C104 = 104;
+    /* 楚雄彝族自治州 */
+    static int C105 = 105;
+    /* 玉溪市 */
+    static int C106 = 106;
+    /* 红河哈尼族彝族自治州 */
+    static int C107 = 107;
+    /* 普洱市 */
+    static int C108 = 108;
+    /* 西双版纳傣族自治州 */
+    static int C109 = 109;
+    /* 临沧市 */
+    static int C110 = 110;
+    /* 大理白族自治州 */
+    static int C111 = 111;
+    /* 保山市 */
+    static int C112 = 112;
+    /* 怒江傈僳族自治州 */
+    static int C113 = 113;
+    /* 丽江市 */
+    static int C114 = 114;
+    /* 迪庆藏族自治州 */
+    static int C115 = 115;
+    /* 德宏傣族景颇族自治州 */
+    static int C116 = 116;
+    /* 张掖市 */
+    static int C117 = 117;
+    /* 武威市 */
+    static int C118 = 118;
+    /* 东莞市 */
+    static int C119 = 119;
+    /* 东沙群岛 */
+    static int C120 = 120;
+    /* 三亚市 */
+    static int C121 = 121;
+    /* 鄂州市 */
+    static int C122 = 122;
+    /* 乌海市 */
+    static int C123 = 123;
+    /* 莱芜市 */
+    static int C124 = 124;
+    /* 海口市 */
+    static int C125 = 125;
+    /* 蚌埠市 */
+    static int C126 = 126;
+    /* 济源市 */
+    static int C1277 = 1277;
+    /* 合肥市 */
+    static int C127 = 127;
+    /* 阜阳市 */
+    static int C128 = 128;
+    /* 芜湖市 */
+    static int C129 = 129;
+    /* 安庆市 */
+    static int C130 = 130;
+    /* 北京市 */
+    static int C131 = 131;
+    /* 重庆市 */
+    static int C132 = 132;
+    /* 南平市 */
+    static int C133 = 133;
+    /* 泉州市 */
+    static int C134 = 134;
+    /* 庆阳市 */
+    static int C135 = 135;
+    /* 定西市 */
+    static int C136 = 136;
+    /* 韶关市 */
+    static int C137 = 137;
+    /* 佛山市 */
+    static int C138 = 138;
+    /* 茂名市 */
+    static int C139 = 139;
+    /* 珠海市 */
+    static int C140 = 140;
+    /* 梅州市 */
+    static int C141 = 141;
+    /* 桂林市 */
+    static int C142 = 142;
+    /* 河池市 */
+    static int C143 = 143;
+    /* 崇左市 */
+    static int C144 = 144;
+    /* 钦州市 */
+    static int C145 = 145;
+    /* 贵阳市 */
+    static int C146 = 146;
+    /* 六盘水市 */
+    static int C147 = 147;
+    /* 秦皇岛市 */
+    static int C148 = 148;
+    /* 沧州市 */
+    static int C149 = 149;
+    /* 石家庄市 */
+    static int C150 = 150;
+    /* 邯郸市 */
+    static int C151 = 151;
+    /* 新乡市 */
+    static int C152 = 152;
+    /* 洛阳市 */
+    static int C153 = 153;
+    /* 商丘市 */
+    static int C154 = 154;
+    /* 许昌市 */
+    static int C155 = 155;
+    /* 襄樊市 */
+    static int C156 = 156;
+    /* 荆州市 */
+    static int C157 = 157;
+    /* 长沙市 */
+    static int C158 = 158;
+    /* 衡阳市 */
+    static int C159 = 159;
+    /* 镇江市 */
+    static int C160 = 160;
+    /* 南通市 */
+    static int C161 = 161;
+    /* 淮安市 */
+    static int C162 = 162;
+    /* 南昌市 */
+    static int C163 = 163;
+    /* 新余市 */
+    static int C164 = 164;
+    /* 通化市 */
+    static int C165 = 165;
+    /* 锦州市 */
+    static int C166 = 166;
+    /* 大连市 */
+    static int C167 = 167;
+    /* 乌兰察布市 */
+    static int C168 = 168;
+    /* 巴彦淖尔市 */
+    static int C169 = 169;
+    /* 渭南市 */
+    static int C170 = 170;
+    /* 宝鸡市 */
+    static int C171 = 171;
+    /* 枣庄市 */
+    static int C172 = 172;
+    /* 日照市 */
+    static int C173 = 173;
+    /* 东营市 */
+    static int C174 = 174;
+    /* 威海市 */
+    static int C175 = 175;
+    /* 太原市 */
+    static int C176 = 176;
+    /* 文山壮族苗族自治州 */
+    static int C177 = 177;
+    /* 温州市 */
+    static int C178 = 178;
+    /* 杭州市 */
+    static int C179 = 179;
+    /* 宁波市 */
+    static int C180 = 180;
+    /* 中卫市 */
+    static int C181 = 181;
+    /* 临夏回族自治州 */
+    static int C182 = 182;
+    /* 辽源市 */
+    static int C183 = 183;
+    /* 抚顺市 */
+    static int C184 = 184;
+    /* 阿坝藏族羌族自治州 */
+    static int C185 = 185;
+    /* 宜宾市 */
+    static int C186 = 186;
+    /* 中山市 */
+    static int C187 = 187;
+    /* 亳州市 */
+    static int C188 = 188;
+    /* 滁州市 */
+    static int C189 = 189;
+    /* 宣城市 */
+    static int C190 = 190;
+    /* 廊坊市 */
+    static int C191 = 191;
+    /* 宁德市 */
+    static int C192 = 192;
+    /* 龙岩市 */
+    static int C193 = 193;
+    /* 厦门市 */
+    static int C194 = 194;
+    /* 莆田市 */
+    static int C195 = 195;
+    /* 天水市 */
+    static int C196 = 196;
+    /* 清远市 */
+    static int C197 = 197;
+    /* 湛江市 */
+    static int C198 = 198;
+    /* 阳江市 */
+    static int C199 = 199;
+    /* 河源市 */
+    static int C200 = 200;
+    /* 潮州市 */
+    static int C201 = 201;
+    /* 来宾市 */
+    static int C202 = 202;
+    /* 百色市 */
+    static int C203 = 203;
+    /* 防城港市 */
+    static int C204 = 204;
+    /* 铜仁地区 */
+    static int C205 = 205;
+    /* 毕节地区 */
+    static int C206 = 206;
+    /* 承德市 */
+    static int C207 = 207;
+    /* 衡水市 */
+    static int C208 = 208;
+    /* 濮阳市 */
+    static int C209 = 209;
+    /* 开封市 */
+    static int C210 = 210;
+    /* 焦作市 */
+    static int C211 = 211;
+    /* 三门峡市 */
+    static int C212 = 212;
+    /* 平顶山市 */
+    static int C213 = 213;
+    /* 信阳市 */
+    static int C214 = 214;
+    /* 鹤壁市 */
+    static int C215 = 215;
+    /* 十堰市 */
+    static int C216 = 216;
+    /* 荆门市 */
+    static int C217 = 217;
+    /* 武汉市 */
+    static int C218 = 218;
+    /* 常德市 */
+    static int C219 = 219;
+    /* 岳阳市 */
+    static int C220 = 220;
+    /* 娄底市 */
+    static int C221 = 221;
+    /* 株洲市 */
+    static int C222 = 222;
+    /* 盐城市 */
+    static int C223 = 223;
+    /* 苏州市 */
+    static int C224 = 224;
+    /* 景德镇市 */
+    static int C225 = 225;
+    /* 抚州市 */
+    static int C226 = 226;
+    /* 本溪市 */
+    static int C227 = 227;
+    /* 盘锦市 */
+    static int C228 = 228;
+    /* 包头市 */
+    static int C229 = 229;
+    /* 阿拉善盟 */
+    static int C230 = 230;
+    /* 榆林市 */
+    static int C231 = 231;
+    /* 铜川市 */
+    static int C232 = 232;
+    /* 西安市 */
+    static int C233 = 233;
+    /* 临沂市 */
+    static int C234 = 234;
+    /* 滨州市 */
+    static int C235 = 235;
+    /* 青岛市 */
+    static int C236 = 236;
+    /* 朔州市 */
+    static int C237 = 237;
+    /* 晋中市 */
+    static int C238 = 238;
+    /* 巴中市 */
+    static int C239 = 239;
+    /* 绵阳市 */
+    static int C240 = 240;
+    /* 广安市 */
+    static int C241 = 241;
+    /* 资阳市 */
+    static int C242 = 242;
+    /* 衢州市 */
+    static int C243 = 243;
+    /* 台州市 */
+    static int C244 = 244;
+    /* 舟山市 */
+    static int C245 = 245;
+    /* 固原市 */
+    static int C246 = 246;
+    /* 甘南藏族自治州 */
+    static int C247 = 247;
+    /* 内江市 */
+    static int C248 = 248;
+    /* 曲靖市 */
+    static int C249 = 249;
+    /* 淮南市 */
+    static int C250 = 250;
+    /* 巢湖市 */
+    static int C251 = 251;
+    /* 黄山市 */
+    static int C252 = 252;
+    /* 淮北市 */
+    static int C253 = 253;
+    /* 三明市 */
+    static int C254 = 254;
+    /* 漳州市 */
+    static int C255 = 255;
+    /* 陇南市 */
+    static int C256 = 256;
+    /* 广州市 */
+    static int C257 = 257;
+    /* 云浮市 */
+    static int C258 = 258;
+    /* 揭阳市 */
+    static int C259 = 259;
+    /* 贺州市 */
+    static int C260 = 260;
+    /* 南宁市 */
+    static int C261 = 261;
+    /* 遵义市 */
+    static int C262 = 262;
+    /* 安顺市 */
+    static int C263 = 263;
+    /* 张家口市 */
+    static int C264 = 264;
+    /* 唐山市 */
+    static int C265 = 265;
+    /* 邢台市 */
+    static int C266 = 266;
+    /* 安阳市 */
+    static int C267 = 267;
+    /* 郑州市 */
+    static int C268 = 268;
+    /* 驻马店市 */
+    static int C269 = 269;
+    /* 宜昌市 */
+    static int C270 = 270;
+    /* 黄冈市 */
+    static int C271 = 271;
+    /* 益阳市 */
+    static int C272 = 272;
+    /* 邵阳市 */
+    static int C273 = 273;
+    /* 湘西土家族苗族自治州 */
+    static int C274 = 274;
+    /* 郴州市 */
+    static int C275 = 275;
+    /* 泰州市 */
+    static int C276 = 276;
+    /* 宿迁市 */
+    static int C277 = 277;
+    /* 宜春市 */
+    static int C278 = 278;
+    /* 鹰潭市 */
+    static int C279 = 279;
+    /* 朝阳市 */
+    static int C280 = 280;
+    /* 营口市 */
+    static int C281 = 281;
+    /* 丹东市 */
+    static int C282 = 282;
+    /* 鄂尔多斯市 */
+    static int C283 = 283;
+    /* 延安市 */
+    static int C284 = 284;
+    /* 商洛市 */
+    static int C285 = 285;
+    /* 济宁市 */
+    static int C286 = 286;
+    /* 潍坊市 */
+    static int C287 = 287;
+    /* 济南市 */
+    static int C288 = 288;
+    /* 上海市 */
+    static int C289 = 289;
+    /* 晋城市 */
+    static int C290 = 290;
+    /* 澳门特别行政区 */
+    static int C2911 = 2911;
+    /* 香港特别行政区 */
+    static int C2912 = 2912;
+    /* 南充市 */
+    static int C291 = 291;
+    /* 丽水市 */
+    static int C292 = 292;
+    /* 绍兴市 */
+    static int C293 = 293;
+    /* 湖州市 */
+    static int C294 = 294;
+    /* 北海市 */
+    static int C295 = 295;
+    /* 海南省直辖县级行政单位 */
+    static int C296 = 296;
+    /* 赤峰市 */
+    static int C297 = 297;
+    /* 六安市 */
+    static int C298 = 298;
+    /* 池州市 */
+    static int C299 = 299;
+    /* 福州市 */
+    static int C300 = 300;
+    /* 惠州市 */
+    static int C301 = 301;
+    /* 江门市 */
+    static int C302 = 302;
+    /* 汕头市 */
+    static int C303 = 303;
+    /* 梧州市 */
+    static int C304 = 304;
+    /* 柳州市 */
+    static int C305 = 305;
+    /* 黔南布依族苗族自治州 */
+    static int C306 = 306;
+    /* 保定市 */
+    static int C307 = 307;
+    /* 周口市 */
+    static int C308 = 308;
+    /* 南阳市 */
+    static int C309 = 309;
+    /* 孝感市 */
+    static int C310 = 310;
+    /* 黄石市 */
+    static int C311 = 311;
+    /* 张家界市 */
+    static int C312 = 312;
+    /* 湘潭市 */
+    static int C313 = 313;
+    /* 永州市 */
+    static int C314 = 314;
+    /* 南京市 */
+    static int C315 = 315;
+    /* 徐州市 */
+    static int C316 = 316;
+    /* 无锡市 */
+    static int C317 = 317;
+    /* 吉安市 */
+    static int C318 = 318;
+    /* 葫芦岛市 */
+    static int C319 = 319;
+    /* 鞍山市 */
+    static int C320 = 320;
+    /* 呼和浩特市 */
+    static int C321 = 321;
+    /* 吴忠市 */
+    static int C322 = 322;
+    /* 咸阳市 */
+    static int C323 = 323;
+    /* 安康市 */
+    static int C324 = 324;
+    /* 泰安市 */
+    static int C325 = 325;
+    /* 烟台市 */
+    static int C326 = 326;
+    /* 吕梁市 */
+    static int C327 = 327;
+    /* 运城市 */
+    static int C328 = 328;
+    /* 广元市 */
+    static int C329 = 329;
+    /* 遂宁市 */
+    static int C330 = 330;
+    /* 泸州市 */
+    static int C331 = 331;
+    /* 天津市 */
+    static int C332 = 332;
+    /* 金华市 */
+    static int C333 = 333;
+    /* 嘉兴市 */
+    static int C334 = 334;
+    /* 石嘴山市 */
+    static int C335 = 335;
+    /* 昭通市 */
+    static int C336 = 336;
+    /* 铜陵市 */
+    static int C337 = 337;
+    /* 肇庆市 */
+    static int C338 = 338;
+    /* 汕尾市 */
+    static int C339 = 339;
+    /* 嘉峪关市 */
+    static int C33 = 33;
+    /* 深圳市 */
+    static int C340 = 340;
+    /* 贵港市 */
+    static int C341 = 341;
+    /* 黔东南苗族侗族自治州 */
+    static int C342 = 342;
+    /* 黔西南布依族苗族自治州 */
+    static int C343 = 343;
+    /* 漯河市 */
+    static int C344 = 344;
+    /* 湖北省直辖县级行政单位 */
+    static int C345 = 345;
+    /* 扬州市 */
+    static int C346 = 346;
+    /* 连云港市 */
+    static int C347 = 347;
+    /* 常州市 */
+    static int C348 = 348;
+    /* 九江市 */
+    static int C349 = 349;
+    /* 金昌市 */
+    static int C34 = 34;
+    /* 萍乡市 */
+    static int C350 = 350;
+    /* 辽阳市 */
+    static int C351 = 351;
+    /* 汉中市 */
+    static int C352 = 352;
+    /* 菏泽市 */
+    static int C353 = 353;
+    /* 淄博市 */
+    static int C354 = 354;
+    /* 大同市 */
+    static int C355 = 355;
+    /* 长治市 */
+    static int C356 = 356;
+    /* 阳泉市 */
+    static int C357 = 357;
+    /* 马鞍山市 */
+    static int C358 = 358;
+    /* 平凉市 */
+    static int C359 = 359;
+    /* 白银市 */
+    static int C35 = 35;
+    /* 银川市 */
+    static int C360 = 360;
+    /* 玉林市 */
+    static int C361 = 361;
+    /* 咸宁市 */
+    static int C362 = 362;
+    /* 怀化市 */
+    static int C363 = 363;
+    /* 上饶市 */
+    static int C364 = 364;
+    /* 赣州市 */
+    static int C365 = 365;
+    /* 聊城市 */
+    static int C366 = 366;
+    /* 忻州市 */
+    static int C367 = 367;
+    /* 临汾市 */
+    static int C368 = 368;
+    /* 达州市 */
+    static int C369 = 369;
+    /* 兰州市 */
+    static int C36 = 36;
+    /* 宿州市 */
+    static int C370 = 370;
+    /* 随州市 */
+    static int C371 = 371;
+    /* 德州市 */
+    static int C372 = 372;
+    /* 恩施土家族苗族自治州 */
+    static int C373 = 373;
+    /* 酒泉市 */
+    static int C37 = 37;
+    /* 大兴安岭地区 */
+    static int C38 = 38;
+    /* 黑河市 */
+    static int C39 = 39;
+    /* 伊春市 */
+    static int C40 = 40;
+    /* 齐齐哈尔市 */
+    static int C41 = 41;
+    /* 佳木斯市 */
+    static int C42 = 42;
+    /* 鹤岗市 */
+    static int C43 = 43;
+    /* 绥化市 */
+    static int C44 = 44;
+    /* 双鸭山市 */
+    static int C45 = 45;
+    /* 鸡西市 */
+    static int C46 = 46;
+    /* 七台河市 */
+    static int C47 = 47;
+    /* 哈尔滨市 */
+    static int C48 = 48;
+    /* 牡丹江市 */
+    static int C49 = 49;
+    /* 大庆市 */
+    static int C50 = 50;
+    /* 白城市 */
+    static int C51 = 51;
+    /* 松原市 */
+    static int C52 = 52;
+    /* 长春市 */
+    static int C53 = 53;
+    /* 延边朝鲜族自治州 */
+    static int C54 = 54;
+    /* 吉林市 */
+    static int C55 = 55;
+    /* 四平市 */
+    static int C56 = 56;
+    /* 白山市 */
+    static int C57 = 57;
+    /* 沈阳市 */
+    static int C58 = 58;
+    /* 阜新市 */
+    static int C59 = 59;
+    /* 铁岭市 */
+    static int C60 = 60;
+    /* 呼伦贝尔市 */
+    static int C61 = 61;
+    /* 兴安盟 */
+    static int C62 = 62;
+    /* 锡林郭勒盟 */
+    static int C63 = 63;
+    /* 通辽市 */
+    static int C64 = 64;
+    /* 海西蒙古族藏族自治州 */
+    static int C65 = 65;
+    /* 西宁市 */
+    static int C66 = 66;
+    /* 海北藏族自治州 */
+    static int C67 = 67;
+    /* 海南藏族自治州 */
+    static int C68 = 68;
+    /* 海东地区 */
+    static int C69 = 69;
+    /* 黄南藏族自治州 */
+    static int C70 = 70;
+    /* 玉树藏族自治州 */
+    static int C71 = 71;
+    /* 果洛藏族自治州 */
+    static int C72 = 72;
+    /* 甘孜藏族自治州 */
+    static int C73 = 73;
+    /* 德阳市 */
+    static int C74 = 74;
+    /* 成都市 */
+    static int C75 = 75;
+    /* 雅安市 */
+    static int C76 = 76;
+    /* 眉山市 */
+    static int C77 = 77;
+    /* 自贡市 */
+    static int C78 = 78;
+    /* 乐山市 */
+    static int C79 = 79;
+    /* 凉山彝族自治州 */
+    static int C80 = 80;
+    /* 攀枝花市 */
+    static int C81 = 81;
+    /* 和田地区 */
+    static int C82 = 82;
+    /* 喀什地区 */
+    static int C83 = 83;
+    /* 克孜勒苏柯尔克孜自治州 */
+    static int C84 = 84;
+    /* 阿克苏地区 */
+    static int C85 = 85;
+    /* 巴音郭楞蒙古自治州 */
+    static int C86 = 86;
+    /* 新疆直辖县级行政单位 */
+    static int C87 = 87;
+    /* 博尔塔拉蒙古自治州 */
+    static int C88 = 88;
+    /* 吐鲁番地区 */
+    static int C89 = 89;
+    /* 伊犁哈萨克自治州 */
+    static int C90 = 90;
+    /* 哈密地区 */
+    static int C91 = 91;
+    /* 乌鲁木齐市 */
+    static int C92 = 92;
+    /* 昌吉回族自治州 */
+    static int C93 = 93;
+    /* 塔城地区 */
+    static int C94 = 94;
+    /* 克拉玛依市 */
+    static int C95 = 95;
+    /* 阿勒泰地区 */
+    static int C96 = 96;
+    /* 山南地区 */
+    static int C97 = 97;
+    /* 林芝地区 */
+    static int C98 = 98;
+    /* 昌都地区 */
+    static int C99 = 99;
+}
